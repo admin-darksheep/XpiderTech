30e5746b3899476ff26862ea275a63b81e8e4c1c
admin-darksheep <150998290+admin-darksheep@users.noreply.github.com>
2024-02-19 04:31:49 +0530

    Add files via upload

--- utils/javascript/merger.js
+++ utils/javascript/merger.js
@@ -1,10 +1,10 @@
 /**
- * utils/merger.js - File responsible for merging data.
- * Developer: Abdul Ahad
- * Last Updated: Abdul Ahad
- * This code follows the GNU General Public License (Version 3).
- * Brand: XpiderTech
- */
+* utils/merger.js - File responsible for merging data.
+* Developer: Abdul Ahad
+* Last Updated: Abdul Ahad
+* This code follows the GNU General Public License (Version 3).
+* Brand: XpiderTech
+*/
 (function(globalNS, factory) {
     if ("undefined" !== typeof module && "object" === typeof module?.exports) {
         module.exports = factory();
@@ -74,7 +74,9 @@
 
         return target;
     }
-    function deepClone(source, options = {}, clonedObject = {}, seen = new WeakSet()) {
+    function deepClone(source, options = {
+        enumerableOnly: true
+    }, clonedObject = {}, seen = new WeakSet()) {
         if (seen.has(source) || arePrimitive(source)) {
             return source;
         }
@@ -90,9 +92,15 @@
         } else if (areRegExp(source)) {
             return cloneRegExp(source, options);
         } else if (areValidObject(source)) {
-            return cloneObject(source, options, clonedObject, seen);
-        }
 
+            return cloneObject(source, options, clonedObject, seen);
+        } else if (areValidFunction(source)) {
+            return cloneFunction(source);
+        } else if (areConstructor(source)) {
+            return cloneConstructor(source, options, seen);
+        } else if (areObject(source) && !areValidObject(source)) {
+            return source.getInstance();
+        }
         return clonedObject;
     }
 
@@ -154,8 +162,31 @@
 
         return clonedObject;
     }
+    function cloneFunction(originalFunction) {
+        return function (...args) {
+            return originalFunction.apply(this, args);
+        };
+    }
+    function cloneConstructor(originalConstructor, options, seen = new WeakSet()) {
+        seen.add(originalConstructor);
+        const clonedConstructor = function (...args) {
+            originalConstructor.apply(this, args);
+        };
 
+        clonedConstructor.prototype = Object.create(originalConstructor.prototype);
 
+        for (const key in originalConstructor) {
+            if (originalConstructor.hasOwnProperty(key)) {
+                if (areObject(originalConstructor[key])) {
+                    clonedConstructor[key] = deepClone(originalConstructor[key], options, seen);
+                } else {
+                    clonedConstructor[key] = originalConstructor[key];
+                }
+            }
+        }
+
+        return clonedConstructor;
+    }
     function every(callbackFn) {
         return Array.prototype.every.call(this, callbackFn);
     }
@@ -195,7 +226,11 @@
         return every.bind(arguments)((arg)=> arg instanceof Map);
     }
     function areValidObject() {
-        return every.bind(arguments)((arg)=>areObject(arg)&&!areArray(arg)&&!/^\u0020*class\u0020+/.test(arg.constructor)&&!areSet(arg)&&!areMap(arg)&&!areRegExp(arg)&&!areHtmlElement(arg)&&!areDateObject(arg));
+        return every.bind(arguments)((arg)=>areObject(arg)&&!areArray(arg)&&!/^\u0020*class\u0020+/.test(arg.constructor)&&!areSet(arg)&&!areMap(arg)&&!areRegExp(arg)&&!areHtmlElement(arg)&&!areDateObject(arg)&&!areValidFunction(arg));
+    }
+    function areValidFunction() {
+        return Array.prototype.every.call(arguments,
+            (arg)=>"function" === typeof arg&&!/^\u0020*class\u0020+/.test(arg.toString()));
     }
     function areArray() {
         return every.bind(arguments)((arg)=> Array.isArray(arg));
@@ -204,6 +239,16 @@
     function areHtmlElement() {
         return every.bind(arguments)((arg)=>arg instanceof HTMLElement);
     }
+    function areConstructor() {
+        return Array.prototype.every.call(arguments, (arg)=> {
+            if ("function" === typeof arg && "object" === typeof (arg.prototype??false)) {
+                return (Object.getOwnPropertyNames(new arg()).length > 0);
+            }
+            return false;
+        });
+    }
+
+
     function defineProperty(object, key, predicate, options = {}) {
         const descriptor =
         (predicate instanceof Object) ? {
@@ -234,7 +279,6 @@
         object[key] = value;
         return true;
     }
-
 
     function ownKeys(object, options) {
         const keys = [
